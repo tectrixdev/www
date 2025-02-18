6d7117af71a0321e187b63927ee2ed7b7bf2ae79
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-02-18 12:02:56 +0100

    add text gradient

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main>
       <div
         id="tagline"
-        className={`${Rubik.className} text-6xl self-center text-center mt-10 lg:text-9xl`}
+        className={`${Rubik.className} bg-gradient-to-r from-red-700 to-blue-800 bg-clip-text text-6xl self-center text-center mt-10 lg:text-9xl md:text-8xl transition-all duration-1000 text-transparent `}
       >
         TecTrix
       </div>
