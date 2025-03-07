a9fa40323a2e132825b14792079702999fe0ad25
TecTrix <joranhennion@duck.com>
2025-03-08 00:29:36 +0100

    ooh fancy :stars:

--- components/banner.tsx
+++ components/banner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner: React.FC = () => {
   return (
-    <div className="bg-yellow-300 p-4 text-center text-black">
+    <div className="bg-black/50 p-4 text-center font-bold text-white backdrop-blur-lg">
       <p>My website is under construction and this is the progress.</p>
       <p>For info and old page go to:</p>
       <a
