f685808f928e6f19b90ffa78fe08fcf427cd80a6
TecTrix <joranhennion@duck.com>
2025-03-07 16:17:32 +0100

    add some things to cards

--- components/card.tsx
+++ components/card.tsx
@@ -14,7 +14,7 @@ const Card: React.FC<Props> = ({ title, content, transparency, color }) => {
       initial={{ opacity: 0, y: 100, scale: 0.9 }}
       whileInView={{ opacity: 100, y: 0, scale: 1 }}
       transition={{ duration: 1.5, type: "spring" }}
-      className="size-40 rounded-lg border-2 border-slate-300 bg-black/50 backdrop-blur-sm"
+      className="h-48 w-full rounded-lg border-2 border-slate-300 bg-black/50 backdrop-blur-md md:h-96 md:w-64"
     ></motion.div>
   );
 };
