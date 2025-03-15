cdb8cb53d7498b45839a1b947ff7a9c10a5e914d
TecTrix <joranhennion@duck.com>
2025-03-15 16:34:05 +0100

    add license link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full bg-white/10 p-4 text-center font-bold text-white backdrop-blur-lg">
-      <p>
-        &copy; {new Date().getFullYear()} Joran Hennion. All rights reserved.
-      </p>
-    </footer>
+    <Link
+      href="https://raw.githubusercontent.com/tectrixdev/portfolio/refs/heads/main/LICENSE"
+      title="License (info)"
+    >
+      <footer className="w-full bg-white/10 p-4 text-center font-bold text-white backdrop-blur-lg">
+        <p>
+          &copy; {new Date().getFullYear()} Joran Hennion. All rights reserved.
+        </p>
+      </footer>
+    </Link>
   );
 };
 
