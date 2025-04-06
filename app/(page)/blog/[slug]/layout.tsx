aeace208cfdde1ae53d32195933c4a8372835f3e
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-04-06 19:11:38 +0200

    make globals css really global and fix scrollbars on firefox

--- app/(page)/blog/[slug]/layout.tsx
+++ app/(page)/blog/[slug]/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./blog.css";
+import "@/app/globals.css";
 import Scrollbody from "@/components/scrollbg";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
