c0b2e0658c9d800f7dbf4e28bb5285687c76d97b
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-04-06 22:30:45 +0200

    finalize home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -12,6 +12,7 @@ import Banner from "@/components/banner";
 import Subtitle from "@/components/subtitle";
 import PictureGallery from "@/components/picturegallery";
 import Navbar from "@/components/navbar";
+import CardWrapper from "@/components/cardwrapper";
 
 const main = Rubik_Glitch({
   subsets: ["latin"],
@@ -25,25 +26,49 @@ export default function Home() {
       <Banner />
       <Tagline line="TecTrix" />
       <Tagsub sub="Joran Hennion" />
-      <Navbar
-        items={[
-          { label: "Blog", url: "/blog" },
-        ]}
-      />
+      <Navbar items={[{ label: "Blog", url: "/blog" }]} />
       <Content>
         <Subtitle title="Photography" />
         <PictureGallery />
         <Subtitle title="Development" />
-        <Card
-          title="My portfolio"
-          description="You are right here ;)"
-          imageUrl="this.webp"
-          projectUrl="https://github.com/tectrixdev/www"
-          button="view source"
-          color={-90}
-        />
-        <Subtitle title="Design" />
+        <CardWrapper>
+          <Card
+            title="My portfolio"
+            description="You are right here ;)"
+            imageUrl="this.webp"
+            projectUrl="https://github.com/tectrixdev/www"
+            button="view source"
+            color={-90}
+          />
+          <Card
+            title="ADHDO"
+            description="a todo app for people with ADHD (unfinished)"
+            imageUrl="adhdo.png"
+            projectUrl="https://github.com/tectrixdev/ADHDO"
+            button="view source"
+            color={-190}
+          />
+        </CardWrapper>
+        {/*<Subtitle title="Design" />*/}
         <Subtitle title="Linux" />
+        <CardWrapper>
+          <Card
+            title="TectrixMC"
+            description="I manage this server which runs on ubuntu server 24.04, website by ilxplay"
+            imageUrl="tectrixmc.png"
+            projectUrl="https://minecraft.tectrix.dev/"
+            button="Join us!"
+            color={130}
+          />
+          <Card
+            title="Arcade uploader"
+            description="A fairly new project which aims to make coding for arcade machines very easy for kids, more info will release soon as I'm working on it!"
+            imageUrl="arcade.png"
+            projectUrl="https://github.com/tectrixdev/arcade_upload"
+            button="view source"
+            color={-130}
+          />
+        </CardWrapper>
       </Content>
     </main>
   );
