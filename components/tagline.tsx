36a609d1c45710d981df156b4e9863952847717d
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-04-07 23:41:11 +0200

    kinda fix tagline overflow on mobile

--- components/tagline.tsx
+++ components/tagline.tsx
@@ -17,11 +17,11 @@ const Tagline: React.FC<Props> = ({ line }) => {
   return (
     <motion.h1
       id="tagline"
-      className={`${main.className} mx-auto mb-5 mt-10 w-fit cursor-grab select-none self-center text-center text-6xl text-white drop-shadow-xl active:cursor-grabbing md:text-7xl lg:text-8xl`}
+      className={`${main.className} mx-auto mb-5 mt-10 w-fit cursor-grab select-none text-center text-6xl text-white drop-shadow-xl active:cursor-grabbing md:text-7xl lg:text-8xl`}
       drag
       dragSnapToOrigin
       dragConstraints={{ top: 10, left: 10, right: 10, bottom: 10 }}
-      whileHover={{ scale: 1.2 }}
+      whileHover={{ scale: 0.9 }}
       whileTap={{ scale: 0.8 }}
       initial={{ y: -100 }}
       whileInView={{ y: 0 }}
