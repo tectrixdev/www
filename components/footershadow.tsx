7a01bce2340e38afbf785b88327560c4efd71835
TecTrix <93141222+tectrixdev@users.noreply.github.com>
2025-04-10 18:47:25 +0200

    fix shadow obstructing issue

--- components/footershadow.tsx
+++ components/footershadow.tsx
@@ -2,7 +2,7 @@ export default function FooterShadow() {
   return (
     <div
       id="fade"
-      className="invisible sticky bottom-0 z-50 h-44 w-full bg-gradient-to-b from-transparent to-black md:visible"
+      className="pointer-events-none invisible sticky bottom-0 z-50 h-44 w-full bg-gradient-to-b from-transparent to-black md:visible"
     />
   );
 }
