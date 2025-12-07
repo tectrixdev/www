e35f168b5a7f441aab26fdbff0ea0166d31aa6b3
tectrixdev <joranhennion@duck.com>
2025-12-07 10:19:09 +0100

    FIX react2shell

--- components/divider.tsx
+++ components/divider.tsx
@@ -8,7 +8,6 @@ export default function Divider() {
 			initial={{ opacity: 0, scale: 0.5 }}
 			whileInView={{ opacity: 1, scale: 1 }}
 			transition={{ duration: 1, type: "spring" }}
-			id="divider"
 			className="mx-auto my-5 h-0.5 w-[60vw] self-center bg-white opacity-65 shadow-lg backdrop-blur-lg lg:h-1"
 		/>
 	);
